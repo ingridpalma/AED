88c3af4a6277bb83bef3bd4b203e838e052b0d25
Vitor Lopes <vitorhugo.lopes@gmail.com>
2019-10-29 07:59:54 -0300

    .

--- heaps/heaps_server/views.py
+++ heaps/heaps_server/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Device
 
-# Create your views here.
+
+def post_list(request):
+    devices = Device.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'heaps_server/device_list.html', {'devices': devices})
+
